5e23aee69b6fc3885a96e9a8e5d1720b545dd4f6
p0rkan0x <khapokela@gmail.com>
2021-06-28 12:24:48 +0530

    first commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Lorem ipsum began as scrambled, nonsensical Latin derived from Cicero's 1st-century BC text De Finibus Bonorum et Malorum.",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh. Neque vitae tempus quam pellentesque nec nam. Neque vitae tempus quam pellentesque nec nam aliquam. Egestas pretium aenean pharetra magna ac...","categories": ["posts"],
+        "tags": [],
+        "url": "/posts/2021/05/21/world.html",
+        "teaser": null
+      },{
+        "title": "hello world is mine",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": ["posts"],
+        "tags": [],
+        "url": "/posts/2021/06/12/hello.html",
+        "teaser": null
+      }]
